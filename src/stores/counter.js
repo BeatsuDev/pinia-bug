bb01b34d0b04cb6cbe535de858c5fc1f69fe88a0
BeatsuDev <ericbieszczad@gmail.com>
2025-03-19 16:53:48 +0100

    add comments to where the "bug" is relevant

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -2,14 +2,14 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
-  const hiddenData = ref(0)
+  const hiddenData = ref(0)  // This data does not persist
   const count = computed(() => hiddenData.value)
 
   function increment() {
     hiddenData.value++
   }
 
-  return { count, increment }
+  return { count, increment } // I could return hiddenData here and it would work, but I don't want to expose it
 },
 {
   persist: true
